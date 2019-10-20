71b00a8e2ad2cc908ffde1c8b1be6ed4fe09ca57
jonhuynh <jonvictory@gmail.com>
2019-10-20 12:10:04 -0600

    added cosnole logs to meet the base requirements of the assignment

--- liri.js
+++ liri.js
@@ -1,6 +1,4 @@
 require("dotenv").config();
-
-
 var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
@@ -22,11 +20,26 @@ var input = process.argv.slice(3).join(" ")
 //         }
 //     }
 // }
+//   * This will show the following information about the song in your terminal/bash window
+
+//* Artist(s)
+
+//* The song's name
+
+//* A preview link of the song from Spotify
+
+//* The album that the song is from
+
+//* If no song is provided then your program will default to "The Sign" by Ace of Base.
 
 spotify
   .search({ type: 'track', query: input })
   .then(function(response) {
-    console.log(response.tracks.items[1].album.release_date);
+    console.log(response)
+    console.log(response.tracks.items[0])
+    console.log(response.tracks.items[0].album.name);
+    console.log(response.tracks.items[0].album.artists[0].name);
+    console.log(response.tracks.items[0].preview_url)
   })
   .catch(function(err) {
     console.log(err);
