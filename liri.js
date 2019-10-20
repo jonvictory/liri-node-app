1a4c5842327b060241c20193e8f37a19d976e318
jonhuynh <jonvictory@gmail.com>
2019-10-20 11:41:54 -0600

    I've got most of the basic structure of the app completed. I can make calls to spotify, retrieve information, I just need to print the correct values. Couldn't commit earlier because I was having problems with .env and I didn't want to publish my keys.

--- liri.js
+++ liri.js
@@ -1,29 +1,33 @@
 require("dotenv").config();
-var keys = require("./keys.js");
+
 
 var axios = require("axios");
 var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
-var input = process.argv
+var input = process.argv.slice(3).join(" ")
 
-var songTit = ""
-if (input[2] == 'spotify-this-song') {
-    for (i = 3; i<input.length; i++) {
-        if (i>3 && i<input.lenth) {
-        songTit = songTit + '+' + input[i]
-        }
-        else {
-            songTit += input[i];
-        }
-    }
-}
 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-   
-  console.log(data); 
+// I started with a loop, but had problems getting the input to parse correctly to use the spotify api. 
+// var songTit = ""
+// if (input[2] == 'spotify-this-song') {
+//     for (i = 3; i<input.length; i++) {
+//         if (i>3 && i<input.lenth) {
+//         songTit = songTit + input[i].join(" ")
+//         }
+//         else {
+//             songTit += input[i];
+//         }
+//     }
+// }
+
+spotify
+  .search({ type: 'track', query: input })
+  .then(function(response) {
+    console.log(response.tracks.items[1].album.release_date);
+  })
+  .catch(function(err) {
+    console.log(err);
   });
